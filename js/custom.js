58c3ca0b2fd07a469586073a4a6fc1787baf67cd
Yess <ycruz@ubicamovil.com>
2026-04-25 01:56:51 -0600

    feat: init repository

---
+++ js/custom.js
@@ -0,0 +1,751 @@
+//Electricidad
+$('#co').magnificPopup({
+    items: [
+      {
+         src: 'images/c1.jpg',
+        title: 'COMPRESOR DE AIRE'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//Electricidad
+$('#col').magnificPopup({
+    items: [
+      {
+         src: 'images/c2.jpg',
+        title: 'COJINETES MOTOR DE GASOLINA'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//Electricidad
+$('#colo').magnificPopup({
+    items: [
+      {
+         src: 'images/c3.jpg',
+        title: 'SERRODIARIOS GASOLINA'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//Electricidad
+$('#colon').magnificPopup({
+    items: [
+      {
+         src: 'images/c4.jpg',
+        title: 'COJINETES MOTOR DE DIESEL'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//catorena
+$('#blocks').magnificPopup({
+    items: [
+      // {
+      //     src: 'images/h1.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/h2.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/h3.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      {
+          src: 'images/b1.jpg',
+        title: 'BLOCKS FRENOS DE AIRE'
+      },
+      {
+          src: 'images/b2.jpg',
+        title: 'BLOCKS FRENOS DE AIRE'
+      },
+      {
+          src: 'images/b3.jpg',
+        title: 'BLOCKS FRENOS DE AIRE'
+      },
+      // {
+      //     src: 'images/g1.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/g2.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/g1.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/g2.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/g3.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/d1.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/d2.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      
+      // {
+      //     src: 'images/g5.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/i3.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/i4.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/h6.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/h7.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+      // {
+      //     src: 'images/h8.jpg',
+      //   title: 'Instalacion Hidrosanitaria'
+      // },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//----------------------------------Carpas-------------------------------//
+$('#carpa_trans').magnificPopup({
+    items: [
+      {
+          src: 'image/productos/carpa_transparente.jpg',
+        title: 'Carpa transparente'
+      },
+      {
+        src: 'image/productos/carpa_transparente_1.jpg',
+      title: 'Carpa transparente'
+    },
+    {
+      src: 'image/productos/carpa_transparente_2.jpg',
+    title: 'Carpa transparente'
+  },
+  {
+    src: 'image/productos/carpa_transparente_3.jpg',
+  title: 'Carpa transparente'
+},    
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#carpa_blanca').magnificPopup({
+    items: [
+      {
+          src: 'image/productos/carpa_blanca_1.jpg',
+        title: 'Carpa blanca'
+      },
+      {
+          src: 'image/productos/carpa_blanca_2.jpg',
+        title: 'Carpa blanca'
+      },
+      {
+        src: 'image/productos/carpa_blanca_3.jpg',
+      title: 'Carpa blanca'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#carpa_negra').magnificPopup({
+    items: [
+      {
+          src: 'image/productos/carpa_negra.jpg',
+        title: 'Carpa negra'
+      },
+      // {
+      //     src: 'images/p5.jpg',
+      //   title: 'Instalaciones de pararrayos'
+      // }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+//------------------------_Mobiliario_----------------------------//
+$('#silla_cross').magnificPopup({
+    items: [
+      {
+         src: 'image/productos/silla_cross_back.jpg',
+        title: 'Silla cross back'
+      },
+       {
+         src: 'image/productos/silla_cross_back_1.jpg',
+        title: 'Silla cross back'
+      },
+      {
+        src: 'image/productos/silla_cross_back_2.jpg',
+       title: 'Silla cross back'
+     },
+     {
+      src: 'image/productos/silla_cross_back_3.jpg',
+     title: 'Silla cross back'
+     }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#silla_avant').magnificPopup({
+    items: [
+      {
+         src: 'image/productos/silla_avant_garde.jpg',
+        title: 'Silla avant garde'
+      },
+       {
+         src: 'image/productos/silla_avant_garde_1.jpg',
+        title: 'Silla avant garde'
+      },
+      {
+        src: 'image/productos/silla_avant_garde_2.jpg',
+       title: 'Silla avant garde'
+     },
+     {
+       src: 'image/productos/silla_avant_garde_3.jpg',
+      title: 'Silla avant garde'
+    }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#silla_rustica').magnificPopup({
+    items: [
+      {
+         src: 'image/productos/silla_rustica.jpg',
+        title: 'Silla rustica'
+      },
+      {
+        src: 'image/productos/silla_rustica_1.jpg',
+       title: 'Silla rustica'
+     }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#periquera_madera').magnificPopup({
+    items: [
+      {
+         src: 'image/productos/periquera_madera.jpg',
+        title: 'Periquera de madera'
+      },
+      {
+        src: 'image/productos/periquera_madera_1.jpg',
+       title: 'Periquera de madera'
+     },
+     {
+      src: 'image/productos/periquera_madera_2.jpg',
+     title: 'Periquera de madera'
+   },
+   {
+    src: 'image/productos/periquera_madera_3.jpg',
+   title: 'Periquera de madera'
+ }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+
+$('#tablon_rustico').magnificPopup({
+items: [
+{
+   src: 'image/productos/tablon_rustico.jpg',
+  title: 'Tablón rustico'
+},
+{
+  src: 'image/productos/tablon_rustico_1.jpg',
+ title: 'Tablón rustico'
+},
+{
+src: 'image/productos/tablon_rustico_2.jpg',
+title: 'Tablón rustico'
+},
+{
+src: 'image/productos/tablon_rustico_3.jpg',
+title: 'Tablón rustico'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#mesa_cuadrada_r').magnificPopup({
+items: [
+{
+ src: 'image/productos/mesa_cuadrada_rustica.jpg',
+title: 'Mesa cuadrada rustica'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//------------------------_Ambientación_-----------------------------_//
+$('#caballete_bienvenida').magnificPopup({
+items: [
+{
+ src: 'image/productos/caballete_bienvenida.jpg',
+title: 'Caballete de bienvenida'
+},
+{
+src: 'image/productos/caballete_bienvenida_1.jpg',
+title: 'Caballete de bienvenida'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#marco_ceremonial').magnificPopup({
+items: [
+{
+ src: 'image/productos/marco_ceremonia.jpg',
+title: 'Marco ceremonial'
+},
+{
+src: 'image/productos/marco_ceremonia_1.jpg',
+title: 'Marco ceremonial'
+},
+{
+src: 'image/productos/marco_ceremonia_2.jpg',
+title: 'Marco ceremonial'
+},
+{
+src: 'image/productos/marco_ceremonia_3.jpg',
+title: 'Marco ceremonial'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//-----------------------Bases decorativas-----------------------------//
+$('#bases').magnificPopup({
+items: [
+{
+  src: 'image/productos/foto_10.jpg',
+title: 'Bases'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//-----------------------_Loza_-----------------------------//
+$('#plato_b_talavera').magnificPopup({
+items: [
+{
+ src: 'image/productos/plato_base_talavera.jpg',
+title: 'Plato base talavera'
+},
+{
+src: 'image/productos/plato_base_talavera_1.jpg',
+title: 'Plato base talavera'
+},
+{
+src: 'image/productos/plato_base_talavera_2.jpg',
+title: 'Plato base talavera'
+},
+{
+src: 'image/productos/plato_base_talavera_3.png',
+title: 'Plato base talavera'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#plato_t_talavera').magnificPopup({
+items: [
+{
+ src: 'image/productos/plato_trinche_talavera.jpg',
+title: 'Plato trinche talavera'
+},
+{
+src: 'image/productos/plato_trinche_1.jpg',
+title: 'Plato trinche talavera'
+},
+{
+src: 'image/productos/plato_trinche_2.jpg',
+title: 'Plato trinche talavera'
+},
+{
+src: 'image/productos/plato_trinche_3.jpg',
+title: 'Plato trinche talavera'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//
+$('#plato_c_redondo').magnificPopup({
+items: [
+{
+ src: 'image/productos/plato_ceramica_redondo.jpg',
+title: 'Plato de ceramica redondo'
+},
+{
+src: 'image/productos/plato_ceramica_redondo_1.jpg',
+title: 'Plato de ceramica redondo'
+},
+{
+src: 'image/productos/plato_ceramica_redondo_2.jpg',
+title: 'Plato de ceramica redondo'
+},
+{
+src: 'image/productos/plato_ceramica_redondo_3.jpg',
+title: 'Plato de ceramica redondo'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#plato_t_barro').magnificPopup({
+items: [
+{
+ src: 'image/productos/plato_trinche_barro.jpg',
+title: 'Plato trinche barro'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//------------------------_Cubiertería_------------------------//
+$('#cubiertos_plateados').magnificPopup({
+items: [
+{
+ src: 'image/productos/cubierto_plateado.jpg',
+title: 'Cubiertos plateados'
+},
+{
+src: 'image/productos/cubierto_plateado_1.png',
+title: 'Cubiertos plateados'
+},
+{
+src: 'image/productos/cubierto_plateado_2.png',
+title: 'Cubiertos plateados'
+},
+{
+src: 'image/productos/cubierto_plateado_3.jpg',
+title: 'Cubiertos plateados'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#cubiertos_dorados').magnificPopup({
+items: [
+{
+ src: 'image/productos/cubiertos_dorados.jpg',
+title: 'Cubiertos dorados'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#cubiertos_g_rose').magnificPopup({
+items: [
+{
+ src: 'image/productos/cubiertos_gold_rose.jpg',
+title: 'Cubiertos gold rose'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#cubiertos_negros').magnificPopup({
+items: [
+{
+ src: 'image/productos/cubiertos_negro.jpg',
+title: 'Cubiertos negros'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//------------------------Cristalería---------------------//
+
+$('#copa_aguatemplado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_agua_templado.png',
+title: 'Copa Agua templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_blacotemplado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_blanco_templado.png',
+title: 'Copa blanco templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_tintotemplado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_tinto_templado.png',
+title: 'Copa Tinto templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_champagetemplado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_champage_templado.png',
+title: 'Copa Champage Templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_m_templado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_martinera_templado.png',
+title: 'Copa Martinera templada'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_c_templado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_cognac_templado.png',
+title: 'Copa Cognac templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#copa_caballito_templado').magnificPopup({
+items: [
+{
+ src: 'image/productos/copa_caballito_templado.png',
+title: 'Copa caballito templado'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#vaso_oldFa').magnificPopup({
+items: [
+{
+ src: 'image/productos/vaso_old_fashioned.png',
+title: 'Vaso old fashioned'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#vaso_highball').magnificPopup({
+items: [
+{
+ src: 'image/productos/vaso_highball.png',
+title: 'Vaso highball'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+
+//----------------------------Manteleria------------------------//
+$('#c_mesa_yute').magnificPopup({
+items: [
+{
+ src: 'image/productos/camino_mesa_yute.png',
+title: 'Camino mesa yute'
+},
+{
+src: 'image/productos/camino_mesa_yute_1.jpg',
+title: 'Camino mesa yute'
+},
+{
+src: 'image/productos/camino_mesa_yute_2.jpg',
+title: 'Camino mesa yute'
+},
+{
+src: 'image/productos/camino_mesa_yute_3.jpg',
+title: 'Camino mesa yute'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#c_mesa_m_rayas').magnificPopup({
+items: [
+{
+ src: 'image/productos/camino_mesa_manta_rayas.png',
+title: 'Camino mesa manta rayas'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#c_mesa_talavera').magnificPopup({
+items: [
+{
+ src: 'image/productos/camino_mesa_talavera.png',
+title: 'Camino mesa talavera'
+},
+{
+src: 'image/productos/camino_mesa_talavera_1.jpg',
+title: 'Camino mesa talavera'
+},
+{
+src: 'image/productos/camino_mesa_talavera_2.jpg',
+title: 'Camino mesa talavera'
+},
+{
+src: 'image/productos/camino_mesa_talavera_3.jpg',
+title: 'Camino mesa talavera'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#servilleta_crudo').magnificPopup({
+items: [
+{
+ src: 'image/productos/servilleta_crudo.png',
+title: 'Servilleta crudo'
+},
+{
+src: 'image/productos/servilleta_crudo_1.png',
+title: 'Servilleta crudo'
+},
+{
+src: 'image/productos/servilleta_crudo_2.png',
+title: 'Servilleta crudo'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+$('#servilleta_negro').magnificPopup({
+items: [
+{
+ src: 'image/productos/servilleta_negro.png',
+title: 'Servilleta negro'
+},
+{
+src: 'image/productos/servilleta_negro_1.png',
+title: 'Servilleta negro'
+},
+{
+src: 'image/productos/servilleta_negro_2.png',
+title: 'Servilleta negro'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
+//----------------------------equipo------------------------//
+$('#equipo').magnificPopup({
+items: [
+{
+ src: 'image/productos/foto_15.jpg',
+title: 'Mini molcajetes'
+}
+],
+gallery: {
+enabled: true
+},
+type: 'image' // this is a default type
+});
